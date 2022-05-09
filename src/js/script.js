271362254f2e9c1103753e8718ae7f5ef2296266
FNO1996@ <100415785+Fno1996@users.noreply.github.com>
2022-05-09 18:04:20 -0300

    ok

--- src/js/script.js
+++ src/js/script.js
@@ -26,15 +26,38 @@ const users = [
         age: 20
     }
 ];
+function ValidateForm(){
+    var id = documento.getElementById('Submit').forma.identificação;
+    documento.getElementById("onsubmit").innerHTML = id;
+}
+    function mostrarEmails() {
+    const lista = users.map(el => el.email);
+    
+    console.log(`A lista de e-mail foi ${lista}`);
+    var input = document.querySelector("#lista");
+    var texto = input.Value;
+    console.log(texto);
+  }
+  
 function validaLogin(param) {
     const login = users.find(el => el.login === param);
-    
+    var input = document.querySelector("#login");
     if (login === undefined) {
-      console.log('Não encontramos esse usuário');
+        alert('Não encontramos esse usuário');
     } else {
-      console.log(`O usuário ${login.login} foi encontrado.`)
+      alert(`O usuário ${login.login} foi encontrado.`)
     }
 }
+function somarIdade() {
+    const idades = users.map(user => user.age);
+    
+    console.log(idades);
+    
+    const soma = idades.reduce((previous, current) => previous + current, 0);
+    
+    console.log(soma);
+  }
+
     /*
         1. Referenciar elementos dos campos do formulário pelo ID
         2. Verificar se a senha e o e-mail estão válidos 
